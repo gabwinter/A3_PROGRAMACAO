390476768d89b1e2bdf80b3437fc8308b6149b64
Wallesu <wallysonsouza4@hotmail.com>
2023-06-19 23:48:39 -0300

    Adicionado validação na classe "Usuario_sem_definicao"
    Adicionado validação nos atributos cpf, nome e email da classe "Usuario_sem_definicao".

--- src/Sistema_Escola/Usuario_sem_definicao.java
+++ src/Sistema_Escola/Usuario_sem_definicao.java
@@ -3,17 +3,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Usuario_sem_definicao {
-// Declarando os atributos
 	
-	    private String matricula;
+	    private int matricula;
 	    private String nomeCompleto;
 	    private String cpf;
 	    private String endereco;
 	    private String email;
 	    private String celular;
 
-	    public Usuario_sem_definicao(String matricula, String nomeCompleto, String cpf, String endereco, String email, String celular) {
-	        this.matricula = matricula;
+	    public Usuario_sem_definicao(String nomeCompleto, String cpf, String endereco, String email, String celular) throws Exception {
+	    	
+	    	
+	    	this.validateEmail(email);
+	    	this.validateNome(nomeCompleto);
+	    	
+	    	//Regex extrair apenas os números do CPF (111.222.333-44 -> 11122233344)
+	    	cpf = cpf.replaceAll("\\D+","");
+	    	this.validateCpf(cpf);
+	    	
 	        this.nomeCompleto = nomeCompleto;
 	        this.cpf = cpf;
 	        this.endereco = endereco;
@@ -23,7 +30,7 @@ public class Usuario_sem_definicao {
 
 	    // Getters
 
-	    public String getMatricula() {
+	    public int getMatricula() {
 	        return matricula;
 	    }
 
@@ -46,5 +53,35 @@ public class Usuario_sem_definicao {
 	    public String getCelular() {
 	        return celular;
 	    }
+	    
+	    private void validateNome(String nome) throws Exception {
+	    	if(nome.length() <= 2) {
+	    		throw new Exception("O nome do usuário deve ter no mínimo 3 caracteres.");
+	    	}
+	    	if(nome.length() > 255) {
+	    		throw new Exception("O nome do usuário deve ter no máximo 255 caracteres.");
+	    	}
+	    }
+	    
+	    private void validateCpf(String cpf) throws Exception {
+	    	if(cpf.length() != 11) {
+	    		throw new Exception("CPF inválido");
+	    	}
+	    }
+	    
+	    private void validateEmail(String email) throws Exception {
+	    	if(email.length() > 320) {
+	    		throw new Exception("Email não pode ultrapassar de 320 caracteres.");
+	    	}
+	    	if(email.length() < 3) {
+	    		throw new Exception("Email deve ter 3 caracteres ou mais.");
+	    	}
+	    	if(!email.contains("@")) {
+	    		throw new Exception("Email inválido.");
+	    	}
+	    }
+	    
+	    
+	    
 	}
 
